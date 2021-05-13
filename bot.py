801b08878f287b4f88aceda885e90dfe343214aa
Joshua inyang <joshuainyang255@gmail.com>
2021-05-13 05:39:53 +0100

    bot automation

--- bot.py
+++ bot.py
@@ -24,6 +24,7 @@ import json
 import pprint
 import csv
 import pandas as pd
+from telebot.apihelper import leave_chat
 
 # Setup Logging
 logger = telebot.logger
@@ -44,6 +45,8 @@ lastUpdate = ''
 
 blacklist=[]
 
+large_text = []
+
 customText = ''
 
 customDocument = ''
@@ -62,21 +65,6 @@ with open('Test.csv', 'r') as file:
 
 
 
-
-
-
-@bot.message_handler(commands=['start', 'help'])
-def start(message):
-    """This initiates the bot into action"""
-    user = message.from_user
-
-    bot.reply_to(message, f'Hey, welcome {user.first_name}.')
-
-
-
-
-
-
 @bot.message_handler(commands=['contentmessage'])
 def start(message):
 
@@ -88,7 +76,8 @@ def start(message):
 
 
     else:
-        bot.reply_to(message, "Hello Dave")
+        bot.reply_to(message, "Hello Dave.")
+        time.sleep(1)
 
         text = "What would you like to send today?"
         mark_up = types.InlineKeyboardMarkup(row_width=2)
@@ -100,12 +89,7 @@ def start(message):
         mark_up.row(c, d)
         quest = bot.send_message(admin, "What would you like to send today?", reply_markup=mark_up)
 
-        # bot.register_next_step_handler(message=quest, callback=customMessage)
-
-
-        # A question to the admin   
-        # question = bot.send_message(admin, "What would you like to send today?")
-        # bot.register_next_step_handler(message=question, callback=customMessage)
+        
 
 
 
@@ -142,7 +126,7 @@ def customDoc(msg):
         customDocument = open('new_doc.pdf', 'rb')
 
 
-
+        time.sleep(1)
         # Ask Admin Confirmation to Send Document 
         question = bot.send_message(
             msg.from_user.id,
@@ -151,6 +135,7 @@ def customDoc(msg):
         )
 
     else:
+        time.sleep(1)
         bot.reply_to(msg, "Wrong input, expected a pdf file. Please send the command(/contentmessage) again.")
         pass
             
@@ -195,7 +180,7 @@ def customImg(msg):
         # customImage = (f'https://api.telegram.org/file/bot{bot_token}/{filePath}')
 
 
-
+        time.sleep(1)
         # Ask Admin Confirmation to Send Document 
         question = bot.send_message(
             msg.from_user.id,
@@ -218,6 +203,7 @@ def customMessage(msg):
     """Confirming Custom Text/Link"""
 
     global customText
+    global large_text
     
     # Adding keyboard custom replies to keyboard
     keyboard = types.InlineKeyboardMarkup(row_width=2)
@@ -225,25 +211,44 @@ def customMessage(msg):
     b = types.InlineKeyboardButton(text="No", callback_data='no')
     keyboard.add(a,b)
 
-    # Save custom message
-    if len(msg.text) > 5000: #sending large messages
-        split_text = util.split_string(customText, 3000) # Split the text each 3000 characters, split_string returns a list with the splitted text.
-        for text in split_text:
-            customText = text
+    if msg.text:
+
+
+
+        # Save custom message
+        if len(str(msg.text)) > 5000: #sending large messages
+            text_file = open('large_text.txt', 'w')
+            n = text_file.write(msg.text)
+            text_file.close()
+
+            large_text = open('large_text.txt', 'rb').read()
+            split_text = util.split_string(large_text, 3000) # Split the text each 3000 characters, split_string returns a list with the splitted text.
+            
+            for text in split_text:
+                large_text.append(text)
+
+            return large_text
+                
+
+        else:
+            customText = msg.text
+        
+        
+        time.sleep(1)
+        # Send Question
+        question = bot.send_message(
+            msg.from_user.id,
+            f"Do you wish to send '{msg.text}' to all stored users in database?",
+            reply_markup=keyboard
+        )
 
     else:
-        customText = msg.text
-    
-    
+        bot.reply_to(msg, "Wrong input, expected an image file. Please send the command(/contentmessage) again.")
 
-    # Send Question
-    question = bot.send_message(
-        msg.from_user.id,
-        f"Do you wish to send '{msg.text}' to all stored users in database?",
-        reply_markup=keyboard
-    )
+        pass
 
-    return customText
+
+    return customText, large_text
 
 
 
@@ -262,14 +267,14 @@ def callback(call):
 
     # check if the input is a document
     if call.data == "docs":
-        
+        time.sleep(1)
         bot.register_next_step_handler(message=forceReply(), callback=customDoc)
     
         
         
     # check if the input is an image
     if call.data == "image":
-        
+        time.sleep(1)
         bot.register_next_step_handler(message=forceReply(), callback=customImg)
         
     # check if the input is a link
@@ -279,7 +284,7 @@ def callback(call):
        
     # check if the input is a text message
     if call.data == "text":
-        
+        time.sleep(1)
         bot.register_next_step_handler(message=forceReply(), callback=customMessage)
 
         
@@ -290,7 +295,7 @@ def callback(call):
         bot.send_message(call.from_user.id, "Thanks for using this service.")
 
 
-
+    
 
 
 def messageUsers():
@@ -305,19 +310,19 @@ def messageUsers():
     bot.send_chat_action(admin, action='typing')
     bot.send_message(admin, "Your custom message is being sent to users in the database.")
 
-    # if customDoc:
-    #     # bot.send_document(1804753795, customDocument)
-    #     [bot.send_document(f"{data}", customDocument) for data in database if data not in blacklist]
+    if customDocument:
+        # bot.send_document(1804i753795, customDocument)
+        [bot.send_document(f"{data}", customDocument) for data in database if data not in blacklist]
 
-    if customImage:
-        [bot.send_photo(f"{data}", customImage) for data in database if data not in blacklist]
+   
 
     elif customMessage:
-        [bot.send_message(f"{data}", f"{customText}  ----- Reply “Unsubsribe” to Unsubscribe from this servie. ") for data in database if data not in blacklist]
+        for text in large_text:
+            [bot.send_message(f"{data}", text) for data in database if data not in blacklist]
 
 
     else:
-        bot.send_message(admin, "Wrong Input")
+        [bot.send_photo(f"{data}", customImage) for data in database if data not in blacklist]
 
     
     
